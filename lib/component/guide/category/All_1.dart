4d25f48eb61e03de819203920dc3af23715483f0
DwKwCs <kwcs_go670@naver.com>
2024-09-09 15:31:19 +0900

    9.9 Firebase 데이터베이스 연결 테스트

--- lib/component/guide/category/All_1.dart
+++ lib/component/guide/category/All_1.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:login_with_pet/const/colors.dart';
 import 'package:percent_indicator/linear_percent_indicator.dart';
+import 'package:login_with_pet/component/guide/guide_card.dart';
 
 class All1 extends StatelessWidget {
 
@@ -39,99 +40,6 @@ class All1 extends StatelessWidget {
             ),
           ],
         ),
-      ),
-    );
-  }
-}
-
-class GuideCard extends StatefulWidget {
-  final String title;
-  final String tag;
-  final double percent;
-
-  const GuideCard({
-    Key? key,
-    required this.title,
-    required this.tag,
-    required this.percent,
-  }) : super(key: key);
-
-  @override
-  GuideCardState createState() => GuideCardState();
-}
-
-class GuideCardState extends State<GuideCard> {
-  bool isSelected = false;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      height: 100,
-      width: MediaQuery.of(context).size.width,
-      padding: EdgeInsets.only(left: 15, right: 15),
-      decoration: BoxDecoration(
-        border: Border(bottom: BorderSide(color: SUB_COLOR2, width: 1)),
-      ),
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          Container(
-            alignment: Alignment.centerLeft,
-            child: TextButton(
-              onPressed: () {},
-              child: Text(
-                widget.title,
-                style: TextStyle(fontSize: 18, fontWeight: FontWeight.w700, color: Colors.black),
-              ),
-            ),
-          ),
-          Transform.translate(
-            offset: Offset(0, -10),
-            child: Container(
-              height: 30,
-              child: Row(
-                children: [
-                  SizedBox(width: 10),
-                  Text(
-                    widget.tag,
-                    style: TextStyle(fontSize: 16, color: Color(0xffcac7c4)),
-                  ),
-                  Spacer(),
-                  IconButton(
-                    icon: Icon(Icons.bookmark_outline_rounded),
-                    iconSize: 30,
-                    isSelected: isSelected,
-                    selectedIcon: Icon(Icons.bookmark_rounded, color: Colors.black),
-                    highlightColor: Colors.transparent,
-                    onPressed: () {
-                      setState(() {
-                        isSelected = !isSelected;
-                      });
-                    },
-                  ),
-                ],
-              ),
-            ),
-          ),
-          Transform.translate(
-            offset: Offset(0, -10),
-            child: Container(
-              child: Row(
-                children: [
-                  LinearPercentIndicator(
-                    width: 180.0,
-                    lineHeight: 8.0,
-                    percent: widget.percent,
-                    barRadius: Radius.circular(10),
-                    backgroundColor: Colors.grey[300],
-                    progressColor: Colors.orange,
-                  ),
-                  Text((widget.percent * 100).toString() + '%'),
-                ],
-              ),
-            ),
-          ),
-        ],
       ),
     );
   }
--- lib/component/guide/guide_card.dart
+++ lib/component/guide/guide_card.dart
@@ -1,15 +1,96 @@
 import 'package:flutter/material.dart';
+import 'package:login_with_pet/const/colors.dart';
+import 'package:percent_indicator/linear_percent_indicator.dart';
 
 class GuideCard extends StatefulWidget {
-  const GuideCard({Key? key}) : super(key: key);
+  final String title;
+  final String tag;
+  final double percent;
+
+  const GuideCard({
+    Key? key,
+    required this.title,
+    required this.tag,
+    required this.percent,
+  }) : super(key: key);
 
   @override
-  State<GuideCard> createState() => _GuideCardState();
+  GuideCardState createState() => GuideCardState();
 }
 
-class _GuideCardState extends State<GuideCard> {
+class GuideCardState extends State<GuideCard> {
+  bool isSelected = false;
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Container(
+      height: 100,
+      width: MediaQuery.of(context).size.width,
+      padding: EdgeInsets.only(left: 15, right: 15),
+      decoration: BoxDecoration(
+        border: Border(bottom: BorderSide(color: SUB_COLOR2, width: 1)),
+      ),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Container(
+            alignment: Alignment.centerLeft,
+            child: TextButton(
+              onPressed: () {},
+              child: Text(
+                widget.title,
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.w700, color: Colors.black),
+              ),
+            ),
+          ),
+          Transform.translate(
+            offset: Offset(0, -10),
+            child: Container(
+              height: 30,
+              child: Row(
+                children: [
+                  SizedBox(width: 10),
+                  Text(
+                    widget.tag,
+                    style: TextStyle(fontSize: 16, color: Color(0xffcac7c4)),
+                  ),
+                  Spacer(),
+                  IconButton(
+                    icon: Icon(Icons.bookmark_outline_rounded),
+                    iconSize: 30,
+                    isSelected: isSelected,
+                    selectedIcon: Icon(Icons.bookmark_rounded, color: Colors.black),
+                    highlightColor: Colors.transparent,
+                    onPressed: () {
+                      setState(() {
+                        isSelected = !isSelected;
+                      });
+                    },
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Transform.translate(
+            offset: Offset(0, -10),
+            child: Container(
+              child: Row(
+                children: [
+                  LinearPercentIndicator(
+                    width: 180.0,
+                    lineHeight: 8.0,
+                    percent: widget.percent,
+                    barRadius: Radius.circular(10),
+                    backgroundColor: Colors.grey[300],
+                    progressColor: Colors.orange,
+                  ),
+                  Text((widget.percent * 100).toString() + '%'),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
